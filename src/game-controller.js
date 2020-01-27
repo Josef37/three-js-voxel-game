ea19c06eb72774618e0392fd1823df2e9f585a72
Josef Wittmann <josefwittmann42@gmail.com>
2020-01-27 09:08:58 +0100

    3D movement without turning

--- src/game-controller.js
+++ src/game-controller.js
@@ -1,26 +1,27 @@
 import { World } from "./world";
 import { Player } from "./player";
 import { GameView } from "./game-view";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const infoBox = document.querySelector("#info");
 
 export class GameController {
   constructor() {
     this.world = new World();
     this.player = new Player(0, 20, 50);
     const canvas = document.querySelector("#c");
-    this.controls = new OrbitControls(this.player.camera, canvas);
-    this.controls.addEventListener("change", () =>
-      this.updatePlayerPosition(this.player.camera.position)
-    );
     this.currentMeshes = [];
     this.view = new GameView(this, canvas, this.player.camera);
   }
 
   animate() {
+    this.player.updatePosition();
     this.view.resize();
     this.view.setCameraPosition(this.player.position);
     this.updateMeshes();
     this.view.draw();
+
+    infoBox.textContent = `${this.player.position.x}, ${this.player.position.y}, ${this.player.position.z}`;
+
     requestAnimationFrame(() => this.animate());
   }
 
--- src/player.js
+++ src/player.js
@@ -1,9 +1,78 @@
-import { Vector3, PerspectiveCamera } from "three";
+import { Vector3, PerspectiveCamera, Clock } from "three";
 
 export class Player {
   constructor(x = 0, y = 0, z = 0) {
     this.position = new Vector3(x, y, z);
     this.camera = new PerspectiveCamera(75, 2, 0.1, 2000);
     this.camera.position.set(this.position.x, this.position.y, this.position.z);
+
+    this.speed = 50;
+    this.keyClocks = {
+      w: new Clock(false),
+      s: new Clock(false),
+      a: new Clock(false),
+      d: new Clock(false),
+      e: new Clock(false),
+      q: new Clock(false)
+    };
+    this.keyDurations = {
+      w: 0,
+      s: 0,
+      a: 0,
+      d: 0,
+      e: 0,
+      q: 0
+    };
+    this.initControls();
+  }
+
+  updatePosition() {
+    for (const key in this.keyClocks) {
+      this.keyDurations[key] += this.keyClocks[key].getDelta();
+    }
+
+    const movementVector = new Vector3();
+    for (const key in this.keyDurations) {
+      const duration = this.keyDurations[key];
+      this.keyDurations[key] = 0;
+      const vector = Player.keyVectors[key].clone().multiplyScalar(duration);
+      movementVector.add(vector);
+    }
+    this.position.add(movementVector.multiplyScalar(this.speed));
+  }
+
+  initControls() {
+    // TODO: Canvas as EventListener?
+    document.addEventListener("keydown", e => {
+      this.startKeyClock(e.key);
+    });
+
+    document.addEventListener("keyup", e => {
+      this.stopKeyClock(e.key);
+    });
+  }
+
+  startKeyClock(key) {
+    const clock = this.keyClocks[key];
+    if (clock) {
+      clock.start();
+    }
+  }
+
+  stopKeyClock(key) {
+    const clock = this.keyClocks[key];
+    if (clock && this.keyDurations.hasOwnProperty(key)) {
+      clock.stop();
+      this.keyDurations[key] += clock.getDelta();
+    }
   }
 }
+
+Player.keyVectors = {
+  w: new Vector3(0, 0, -1),
+  s: new Vector3(0, 0, 1),
+  a: new Vector3(-1, 0, 0),
+  d: new Vector3(1, 0, 0),
+  e: new Vector3(0, -1, 0),
+  q: new Vector3(0, 1, 0)
+};
